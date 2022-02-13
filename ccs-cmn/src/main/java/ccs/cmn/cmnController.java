4c8c88f628afca9be91b730a6e0ac1a34c1cbeb0
nammom <a75755959@gmail.com>
2022-02-13 15:43:47 +0900

    mybatis 설정

--- ccs-cmn/src/main/java/ccs/cmn/cmnController.java
+++ ccs-cmn/src/main/java/ccs/cmn/cmnController.java
@@ -1,15 +1,26 @@
 package ccs.cmn;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ccs.cmn.service.SampleService;
+import ccs.cmn.service.impl.SampleServiceImpl;
 
 @Controller
 public class cmnController {
 
+	@Resource(name="SampleService")
+	private SampleService sampleservice;
 	
 	@RequestMapping(value = "/test")
-	public String cmn() throws Exception {
-		
+	public String cmn(){
+		List<Map<String,Object>> result = sampleservice.selectSampleSql();
+		System.out.println(result);
 		
 		return "cmn/test";
 	}
