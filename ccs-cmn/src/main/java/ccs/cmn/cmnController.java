c39f533536aa0a2a579628e751556808e568a79e
nammom <a75755959@gmail.com>
2022-04-24 20:08:39 +0900

    slf4j추가- logger

--- ccs-cmn/src/main/java/ccs/cmn/cmnController.java
+++ ccs-cmn/src/main/java/ccs/cmn/cmnController.java
@@ -11,14 +11,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import ccs.cmn.service.SampleService;
 import ccs.cmn.service.impl.SampleServiceImpl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Controller
 public class cmnController {
+
+	private final Logger LOGGER = LoggerFactory.getLogger(cmnController.class.getName());
 
 	@Resource(name="SampleService")
 	private SampleService sampleservice;
 	
 	@RequestMapping(value = "/test")
 	public String cmn(){
+		LOGGER.debug("/test 컨트롤러 진입");
+/*		try {
+			
+		}catch(Exception e) {
+			LOGGER.warn("msg", e);
+		}
+*/
 		return "cmn/test";
 	}
 	
