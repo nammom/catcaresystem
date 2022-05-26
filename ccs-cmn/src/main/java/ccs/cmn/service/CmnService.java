2d6bcf76702fd74ac2a48b18cf30bd7e450336ad
nammom <a75755959@gmail.com>
2022-05-26 15:48:31 +0900

    remove cache

--- ccs-cmn/src/main/java/ccs/cmn/service/CmnService.java
+++
@@ -1,9 +0,0 @@
-package ccs.cmn.service;
-
-import java.util.List;
-import java.util.Map;
-
-public interface CmnService {
-
-	public List<Map<String, Object>> selectCmnCodeList(Map<String, Object> param);
-}
--- ccs-framework/src/main/java/ccs/framework/dataservice/BeanService.java
+++
@@ -1,8 +0,0 @@
-package ccs.framework.dataservice;
-
-public class BeanService {
-
-	public static <T> T getBeanService(String beanName,Class<T> type){
-		return type.cast(ApplicationContextProvider.getApplicationContext().getBean(beanName));
-	}
-}
